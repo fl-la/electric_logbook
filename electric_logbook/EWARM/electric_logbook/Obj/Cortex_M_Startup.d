562314859f91413d4d3a3cdd115eebad0808090e
Florian Laufenboeck <florian@laufenbock.de>
2018-10-21 10:12:18 +0200

    remove binary

--- electric_logbook/EWARM/electric_logbook/Obj/Cortex_M_Startup.d
+++
@@ -1,2 +0,0 @@
-electric_logbook/Obj/Cortex_M_Startup.o: \
- /home/florian/bin/SEGGER/arm_segger_embedded_studio_352a_linux_x64/samples/Cortex_M_Startup.s
--- electric_logbook/EWARM/electric_logbook/Obj/SEGGER_THUMB_Startup.d
+++
@@ -1,2 +0,0 @@
-electric_logbook/Obj/SEGGER_THUMB_Startup.o: \
- /home/florian/bin/SEGGER/arm_segger_embedded_studio_352a_linux_x64/samples/SEGGER_THUMB_Startup.s
